84b988e06d0ae5f1fd3f09728d9dd9591f213919
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2023-04-25 19:04:52 +0300

    feat: true init on vuejs

---
+++ LatvianMaps/src/components/Main.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+import L from 'Leaflet';
+export default {
+  data: () => ({
+    theMap: null,
+  }),
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init() {
+      // To place map on middle of Latvia
+      // Info is got from here - https://en.wikipedia.org/wiki/Module:Location_map/data/Latvia
+      const centerOfLatviaDDN = 57;
+      const centerOfLatviaDDE = 24.55;
+      const defaultZoomLevel = 7;
+      var map = L.map('map').setView([centerOfLatviaDDN, centerOfLatviaDDE], defaultZoomLevel);
+
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+    }
+  },
+}
+</script>
+
+<style scoped>
+#map {
+  width: 1024px; /*make them constants? */
+  height: 576px;
+}
+</style>
