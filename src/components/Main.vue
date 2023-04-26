b17bd680f51910970d1e7e81dd009310a08a46c3
Dainis Verdins <55252915+DainisVerdins@users.noreply.github.com>
2023-04-26 21:07:11 +0300

    feat: constant files and searching field

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,36 @@
 <template>
   <div class="map-container">
-    <div id="map" />
+    <div id="map"></div>
     <div class="input-group mb-3 mt-3 px-3">
-      <input type="text" class="form-control" placeholder="Apdzivotas vietas nosaukums" v-model="searchText">
+      <input
+        type="text"
+        class="form-control"
+        placeholder="Apdzivotas vietas nosaukums"
+        v-model="searchText"
+      >
       <div class="input-group-append ">
-        <button class="btn btn-primary" @click="searchPlaces" type="button">Search</button>
-        <button class="btn btn-secondary" @click="searchPlaces" type="button">Zoom result</button>
+        <button
+          class="btn btn-primary"
+          @click="searchPlaces"
+          type="button"
+          >
+            Meklēt
+        </button>
+        <button 
+          class="btn btn-success"
+          @click="zoomToPlace"
+          type="button"
+          :disabled="haveSearchResults()"
+          >
+            Zoom rezultātu
+          </button>
+        <button
+          class="btn btn-info"
+          @click="toDefaults"
+          type="button"
+        >
+          Atiestatīt visu
+        </button>
       </div>
     </div>
   </div>
@@ -14,12 +39,15 @@
 <script>
 import L from 'Leaflet';
 import * as csv from 'jquery-csv';
+import CONSTANT from '../constants'
 
 export default {
   data: () => ({
-    theMap: null,
+    map: null,
     defaultPlaces: null,
     searchText: null,
+    residentialPlacesReflectedOnMapMarkers: [],
+    residentialPlacesReflectedOnMap: [],
 
     /* Record of living Place
       {
@@ -39,29 +67,24 @@ export default {
 
   async mounted() {
     await this.init();
-
   },
+
   methods: {
     async init() {
-      // To place map on middle of Latvia
-      // Info is got from here - https://en.wikipedia.org/wiki/Module:Location_map/data/Latvia
-      const centerOfLatviaDDN = 57;
-      const centerOfLatviaDDE = 24.55;
-      const defaultZoomLevel = 7;
-      this.theMap = L.map('map').setView([centerOfLatviaDDN, centerOfLatviaDDE], defaultZoomLevel);
+      this.mapHandler = L.map('map');
+      this.setDefaultView();
 
       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }).addTo(this.theMap);
+      }).addTo(this.mapHandler);
 
       await this.initDefaultPlaces();
 
       this.placeCornerResidentPlacesOnMap();
-
     },
 
     async initDefaultPlaces() {
-      const response = await fetch('../src/datafiles/AW_VIETU_CENTROIDI.CSV');
+      const response = await fetch(`${CONSTANT.CSV_FILE.PATH}/${CONSTANT.CSV_FILE.FILE_NAME}`);
       const rawCSV = await response.text();
       /*
         According wikipedia living places are vilages, cities, 
@@ -69,7 +92,11 @@ export default {
         So filtering to only contain living places
         Non Living spaces have 2 and more words in it
       */
-      this.defaultPlaces = await csv.toObjects(rawCSV, { separator: ';', delimiter: '#', onParseValue: csv.hooks.castToScalar }).filter(record => !record.NOSAUKUMS.includes(" "));
+      this.defaultPlaces = await csv.toObjects(rawCSV, { separator: CONSTANT.CSV_FILE.SEPARATOR, delimiter: CONSTANT.CSV_FILE.DELIMETER, onParseValue: csv.hooks.castToScalar }).filter(record => !record.NOSAUKUMS.includes(" "));
+    },
+
+    haveSearchResults() {
+      return !this.residentialPlacesReflectedOnMap.length;
     },
 
     placeCornerResidentPlacesOnMap() {
@@ -78,19 +105,20 @@ export default {
       let easternResidentPlace = this.defaultPlaces[0];
       let westernResidentPlace = this.defaultPlaces[0];
 
-      for (let i = 0; i < this.defaultPlaces.length; i++) {
-        if (westernResidentPlace.DD_E < this.defaultPlaces[i].DD_E)
-          westernResidentPlace = this.defaultPlaces[i];
+      for (const place of this.defaultPlaces) {
+        if (westernResidentPlace.DD_E < place.DD_E)
+          westernResidentPlace = place;
 
-        if (easternResidentPlace.DD_E > this.defaultPlaces[i].DD_E)
-          easternResidentPlace = this.defaultPlaces[i];
+        if (easternResidentPlace.DD_E > place.DD_E)
+          easternResidentPlace = place;
 
-        if (southestResidentPlace.DD_N > this.defaultPlaces[i].DD_N)
-          southestResidentPlace = this.defaultPlaces[i];
+        if (southestResidentPlace.DD_N > place.DD_N)
+          southestResidentPlace = place;
 
-        if (northestResidentPlace.DD_N < this.defaultPlaces[i].DD_N)
-          northestResidentPlace = this.defaultPlaces[i];
+        if (northestResidentPlace.DD_N < place.DD_N)
+          northestResidentPlace = place;
       }
+
       // for popup to be more informative
       southestResidentPlace.DEBESPUSE = 'Dienvidi';
       northestResidentPlace.DEBESPUSE = 'Ziemeļi';
@@ -98,23 +126,61 @@ export default {
       westernResidentPlace.DEBESPUSE = 'Rietumi';
       const farthestResidentPlaces = [southestResidentPlace, northestResidentPlace, easternResidentPlace, westernResidentPlace];
 
-      for (let i = 0; i < farthestResidentPlaces.length; i++) {
-        L.marker([farthestResidentPlaces[i].DD_N, farthestResidentPlaces[i].DD_E]).addTo(this.theMap)
-          .bindPopup(`Tālākais apdzīvotais punkts uz ${farthestResidentPlaces[i].DEBESPUSE}em.<br>Apdz vietas nosaukums: ${farthestResidentPlaces[i].NOSAUKUMS}.`)
+      for (const place of farthestResidentPlaces) {
+        L.marker([place.DD_N, place.DD_E]).addTo(this.mapHandler)
+          .bindPopup(`Tālākais apdzīvotais punkts uz ${place.DEBESPUSE}em.<br>Apdz vietas nosaukums: ${place.NOSAUKUMS}.`)
           .openPopup();
       }
     },
 
     searchPlaces() {
-      const output = this.defaultPlaces.filter((place) => {
+      this.clearUserMapMarks();
+
+      const filteredPlaces = this.defaultPlaces.filter((place) => {
         return place.NOSAUKUMS && place.NOSAUKUMS.toLowerCase().includes(this.searchText.toLowerCase());
       });
 
-      for (let i = 0; i < output.length; i++) {
-        L.marker([output[i].DD_N, output[i].DD_E]).addTo(this.theMap)
-          .bindPopup(`Rezultāts .<br>Apdz vietas nosaukums: ${output[i].NOSAUKUMS}.`)
+      // TODO: Probably need to figure out more user friendly marker
+      const greenIcon = new L.Icon({
+        iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+      });
+
+      for (const place of filteredPlaces) {
+        const marker = L.marker([place.DD_N, place.DD_E], { icon: greenIcon }).addTo(this.mapHandler)
+          .bindPopup(`Rezultāts .<br>Apdz vietas nosaukums: ${place.NOSAUKUMS}.`)
           .openPopup();
+
+        this.residentialPlacesReflectedOnMapMarkers.push(marker);
+        this.residentialPlacesReflectedOnMap.push(place);
       }
+    },
+
+    zoomToPlace() {
+      const placeToZoom = this.residentialPlacesReflectedOnMap[0]; // TODO now if have multiple results show one need to make option on click move to next place
+      this.mapHandler.setView([placeToZoom.DD_N, placeToZoom.DD_E], CONSTANT.DEFAULT_VIEW.PLACE_ZOOM);
+    },
+
+    toDefaults() {
+      this.setDefaultView();
+      this.clearUserMapMarks();
+      this.searchText='';
+    },
+
+    setDefaultView() {
+      this.mapHandler.setView([CONSTANT.DEFAULT_VIEW.LATITUDE, CONSTANT.DEFAULT_VIEW.LONGITUDE], CONSTANT.DEFAULT_VIEW.ZOOM);
+    },
+
+    clearUserMapMarks() {
+      for (const placePopup of this.residentialPlacesReflectedOnMapMarkers)
+        this.mapHandler.removeLayer(placePopup);
+
+      this.residentialPlacesReflectedOnMapMarkers.length = 0;
+      this.residentialPlacesReflectedOnMap.length = 0;
     }
   },
 }
---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const CONSTANT = Object.freeze({
+    CSV_FILE: Object.freeze({
+        PATH :'src/datafiles',
+        FILE_NAME : 'AW_VIETU_CENTROIDI.CSV',
+        DELIMETER : '#',
+        SEPARATOR : ';'
+      }),
+    DEFAULT_VIEW: Object.freeze({
+        PLACE_ZOOM : 13,
+        // To place map on middle of Latvia
+        // Info is got from here - https://en.wikipedia.org/wiki/Module:Location_map/data/Latvia
+        ZOOM : 7,
+        LATITUDE : 57,
+        LONGITUDE : 24.55
+      }),
+  });
+
+  export default CONSTANT;
